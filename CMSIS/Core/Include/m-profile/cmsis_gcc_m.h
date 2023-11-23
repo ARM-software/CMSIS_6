7002cdf0d8d32711f16635268d2315fef66e249b
MiloradCvjetkovic <milorad.cvjetkovic@arm.com>
2023-11-23 10:33:37 +0100

    Core: Unify __NO_INIT macro across toolchains

--- CMSIS/Core/Include/m-profile/cmsis_gcc_m.h
+++ CMSIS/Core/Include/m-profile/cmsis_gcc_m.h
@@ -2,7 +2,7 @@
  * @file     cmsis_gcc_m.h
  * @brief    CMSIS compiler GCC header file
  * @version  V6.0.1
- * @date     11. October 2023
+ * @date     23. November 2023
  ******************************************************************************/
 /*
  * Copyright (c) 2009-2023 Arm Limited. All rights reserved.
@@ -111,7 +111,7 @@
   #define __COMPILER_BARRIER()                   __ASM volatile("":::"memory")
 #endif
 #ifndef __NO_INIT
-  #define __NO_INIT                              __attribute__ ((section (".bss.noinit")))
+  #define __NO_INIT                              __attribute__ ((section (".noinit")))
 #endif
 #ifndef __ALIAS
   #define __ALIAS(x)                             __attribute__ ((alias(x)))
