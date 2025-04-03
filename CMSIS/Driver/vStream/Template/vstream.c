241134e363ac716ebd2c8864143f73a51e21ac6f
MiloradCvjetkovic <milorad.cvjetkovic@arm.com>
2025-04-03 08:17:16 +0200

    Update vStream template

--- CMSIS/Driver/vStream/Template/vstream.c
+++ CMSIS/Driver/vStream/Template/vstream.c
@@ -28,16 +28,36 @@
 #include "RTE_Components.h"
 #include CMSIS_device_header
 
+// Add user includes here:
+
+
+// Variables
+
+static vStreamEvent_t fn_event_cb;      // Event handling callback function
+
+// Add user types and variables here:
+
 
 // Functions
 
+// Add user functions here:
+
 static int32_t Initialize (vStreamEvent_t event_cb) {
   (void)event_cb;
+
+  fn_event_cb = event_cb;
+
+  // Add user code here:
 
   return VSTREAM_OK;
 }
 
 static int32_t Uninitialize (void) {
+
+  fn_event_cb = NULL;
+
+  // Add user code here:
+
   return VSTREAM_OK;
 }
 
@@ -46,20 +66,30 @@ static int32_t SetBuf (void *buf, uint32_t buf_size, uint32_t block_size) {
   (void)buf_size;
   (void)block_size;
 
+  // Add user code here:
+
   return VSTREAM_OK;
 }
 
 int32_t Start (uint32_t mode) {
   (void)mode;
 
+  // Add user code here:
+
   return VSTREAM_OK;
 }
 
 static int32_t Stop (void) {
+
+  // Add user code here:
+
   return VSTREAM_OK;
 }
 
 static void *GetBlock (void) {
+
+  // Add user code here:
+
   return NULL;
 }
 
@@ -70,16 +100,22 @@ static int32_t ReleaseBlock (void) {
 static vStreamStatus_t GetStatus (void) {
   vStreamStatus_t stat = { 0U, 0U, 0U, 0U, 0U };
 
+  // Add user code here:
+
   return stat;
 }
 
 static void vStreamEvent (uint32_t event_flags) {
   (void)event_flags;
+
+  // Add user code here:
 }
 
 
 // Driver structure
 
+extern \
+vStreamDriver_t Driver_vStreamDevice;
 vStreamDriver_t Driver_vStreamDevice = {
   Initialize,
   Uninitialize,
