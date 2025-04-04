a96b7fd56f2ca36cf93c9863727d44f99aae0f9f
MiloradCvjetkovic <milorad.cvjetkovic@arm.com>
2025-04-04 08:14:56 +0200

    Updated vStream template

--- CMSIS/Driver/vStream/Template/vstream.c
+++ CMSIS/Driver/vStream/Template/vstream.c
@@ -42,9 +42,14 @@ static vStreamEvent_t fn_event_cb;      // Event handling callback function
 
 // Add user functions here:
 
+/**
+  \fn           int32_t Initialize (vStreamEvent_t event_cb)
+  \brief        Initialize Virtual Streaming interface.
+  \return       VSTREAM_OK on success; otherwise, an appropriate error code
+*/
 static int32_t Initialize (vStreamEvent_t event_cb) {
-  (void)event_cb;
 
+  // Register event callback function
   fn_event_cb = event_cb;
 
   // Add user code here:
@@ -52,8 +57,14 @@ static int32_t Initialize (vStreamEvent_t event_cb) {
   return VSTREAM_OK;
 }
 
+/**
+  \fn           int32_t Uninitialize (void)
+  \brief        De-initialize Virtual Streaming interface.
+  \return       VSTREAM_OK on success; otherwise, an VSTREAM_ERROR error code
+*/
 static int32_t Uninitialize (void) {
 
+  // De-register event callback function
   fn_event_cb = NULL;
 
   // Add user code here:
@@ -61,6 +72,14 @@ static int32_t Uninitialize (void) {
   return VSTREAM_OK;
 }
 
+/**
+  \fn           int32_t SetBuf (void *buf, uint32_t buf_size, uint32_t block_size)
+  \brief        Set Virtual Streaming data buffer.
+  \param[in]    buf             pointer to memory buffer used for streaming data
+  \param[in]    buf_size        total size of the streaming data buffer (in bytes)
+  \param[in]    block_size      streaming data block size (in bytes)
+  \return       VSTREAM_OK on success; otherwise, an appropriate error code
+*/
 static int32_t SetBuf (void *buf, uint32_t buf_size, uint32_t block_size) {
   (void)buf;
   (void)buf_size;
@@ -71,14 +90,29 @@ static int32_t SetBuf (void *buf, uint32_t buf_size, uint32_t block_size) {
   return VSTREAM_OK;
 }
 
-int32_t Start (uint32_t mode) {
-  (void)mode;
+/**
+  \fn           int32_t Start (uint32_t mode)
+  \brief        Start streaming.
+  \param[in]    mode            streaming mode
+  \return       VSTREAM_OK on success; otherwise, an appropriate error code
+*/
+static int32_t Start (uint32_t mode) {
+
+  // Check parameters
+  if ((mode != VSTREAM_MODE_CONTINUOUS) && (mode != VSTREAM_MODE_SINGLE)) {
+    return VSTREAM_ERROR_PARAMETER;
+  }
 
   // Add user code here:
 
   return VSTREAM_OK;
 }
 
+/**
+  \fn           int32_t Stop (void)
+  \brief        Stop streaming.
+  \return       VSTREAM_OK on success; otherwise, an VSTREAM_ERROR error code
+*/
 static int32_t Stop (void) {
 
   // Add user code here:
@@ -86,6 +120,11 @@ static int32_t Stop (void) {
   return VSTREAM_OK;
 }
 
+/**
+  \fn           void *GetBlock (void)
+  \brief        Get pointer to Virtual Streaming data block.
+  \return       pointer to data block, returns NULL if no block is available
+*/
 static void *GetBlock (void) {
 
   // Add user code here:
@@ -93,10 +132,20 @@ static void *GetBlock (void) {
   return NULL;
 }
 
+/**
+  \fn           int32_t ReleaseBlock (void)
+  \brief        Release Virtual Streaming data block.
+  \return       VSTREAM_OK on success; otherwise, an VSTREAM_ERROR error code
+*/
 static int32_t ReleaseBlock (void) {
   return VSTREAM_OK;
 }
 
+/**
+  \fn           vStreamStatus_t GetStatus (void)
+  \brief        Get Virtual Streaming status.
+  \return       streaming status structure
+*/
 static vStreamStatus_t GetStatus (void) {
   vStreamStatus_t stat = { 0U, 0U, 0U, 0U, 0U };
 
@@ -105,7 +154,12 @@ static vStreamStatus_t GetStatus (void) {
   return stat;
 }
 
-static void vStreamEvent (uint32_t event_flags) {
+/**
+  \fn           void EventCallback (uint32_t event_flags)
+  \brief        Callback function for handling Virtual Streaming events.
+  \param[in]    event_flags     bitmask indicating one or more streaming events
+*/
+static void EventCallback (uint32_t event_flags) {
   (void)event_flags;
 
   // Add user code here:
